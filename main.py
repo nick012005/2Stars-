32dea2cc454d1d8a37dc13ef40beb56ab447915f
nick012005 <69267618+nick012005@users.noreply.github.com>
2021-01-05 20:03:13 +0300

    [DeadGameMode is ready]

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ cell_sprites = pygame.sprite.Group()
 
 
 class Direction(Enum):
-    NOBODY = 0
-    ORANGE = 1
-    BLUE = 2
-    
+    NONE = 0
+    NOBODY = 1
+    ORANGE = 2
+    BLUE = 3
+
 
 def load_image(name, color_key=0):
     full_name = os.path.join('images', name)
@@ -39,6 +40,21 @@ class DefaultCell(ABC):
             cell_sprites.remove(self.sprite)
         self.sprite = pygame.sprite.Sprite()
         self.sprite.image = load_image(f"{name[:-4].lower()}{self.direction.name}.png", color_key=-1)
+        self.sprite.image = pygame.transform.scale(self.sprite.image, (self.board.cell_size, self.board.cell_size))
+        self.sprite.rect = (self.x, self.y)
+        cell_sprites.add(self.sprite)
+
+
+class DeadCell(DefaultCell):
+    def __init__(self, x: int, y: int, direction: Direction, board):
+        super().__init__(x, y, direction, board)
+        self.init_image('deadCell')
+
+    def init_image(self, name):
+        if self.sprite:
+            cell_sprites.remove(self.sprite)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image(f"{name}.png", color_key=-1)
         self.sprite.image = pygame.transform.scale(self.sprite.image, (self.board.cell_size, self.board.cell_size))
         self.sprite.rect = (self.x, self.y)
         cell_sprites.add(self.sprite)
@@ -131,6 +147,7 @@ class TowerCell(ProtectedCell):
 
 all_cells = [RandomCell, EmptyCell, TowerCell, BombCell]
 directions = [Direction.BLUE, Direction.ORANGE]
+current_direction = Direction.ORANGE
 
 
 class AddingCell(ClickableCell):
@@ -171,41 +188,58 @@ class AddingCell(ClickableCell):
             self.init_image(self.new_cell.__name__)
 
 
-class Board:
+class DefaultBoard:
     def __init__(self, side_size: int):
-        self.current_direction = Direction.ORANGE
         self.side_size = side_size
+        self.left, self.top, self.cell_distance, self.cell_size = self.set_board()
+        side_range = range(self.side_size)
+        self.table = [[EmptyCell(x, y, Direction.NOBODY, self) for x in side_range] for y in side_range]
+        self.create_capitals(distance=3)
+        self.added = [AddingCell(x + side_size + 2, 6, current_direction, self) for x in range(3)]
+        self.current_direction = Direction.ORANGE
         self.winner = None
-        self.left = pygame.display.get_window_size()[0] // 20
-        self.cell_distance = (pygame.display.get_window_size()[0] // 2 - self.left) // (self.side_size * 10)
-        self.cell_size = (pygame.display.get_window_size()[1] - self.left) // self.side_size - self.cell_distance
-        self.top = (pygame.display.get_window_size()[1] - (self.cell_size + self.cell_distance) * self.side_size) // 2
-        self.table = [[EmptyCell(x, y, Direction.NOBODY, self)
-                       for x in range(self.side_size)] for y in range(self.side_size)]
-        self.added = [AddingCell(x, 6, self.current_direction, self)
-                      for x in range(self.side_size + 2, self.side_size + 5)]
-        self.first_capital_coordinates = None
-        self.second_capital_coordinates = None
+
+    def is_win(self):
+        is_blue_capital_alive = False
+        is_orange_capital_alive = False
+        for row in self.table:
+            for cell in row:
+                if type(cell) == CapitalCell:
+                    if cell.direction == Direction.BLUE:
+                        is_blue_capital_alive = True
+                    else:
+                        is_orange_capital_alive = True
+        if is_orange_capital_alive and is_blue_capital_alive:
+            return None
+        else:
+            if is_orange_capital_alive:
+                self.winner = Direction.ORANGE
+            else:
+                self.winner = Direction.BLUE
+            return True
+
+    def set_board(self):
+        window_width = pygame.display.get_window_size()[0]
+        window_height = pygame.display.get_window_size()[1]
+        left = window_width // 20
+        cell_distance = (window_width // 2 - left) // (self.side_size * 10)
+        cell_size = (window_height - left) // self.side_size - cell_distance
+        top = (window_height - (cell_size + cell_distance) * self.side_size) // 2
+        return left, top, cell_distance, cell_size
 
     def change_current_direction(self):
         self.current_direction = directions[(directions.index(self.current_direction) + 1) % 2]
         self.added = [AddingCell(x, 6, self.current_direction, self)
                       for x in range(self.side_size + 2, self.side_size + 5)]
 
-    def create_capitals(self):
-        first_x, first_y = self.first_capital_coordinates
-        second_x, second_y = self.second_capital_coordinates
-        self.table[first_y][first_x] = CapitalCell(first_x, first_y, Direction.BLUE, self)
-        self.table[second_y][second_x] = CapitalCell(second_x, second_y, Direction.ORANGE, self)
-
-    def generate_capitals_coordinates(self, distance: int):
+    def create_capitals(self, distance: int):
         first_x, second_x = random.choices(range(self.side_size), k=2)
         first_y, second_y = random.choices(range(self.side_size), k=2)
         if abs(first_x - second_x) > distance and abs(first_y - second_y) > distance:
-            self.first_capital_coordinates = (first_x, first_y)
-            self.second_capital_coordinates = (second_x, second_y)
+            self.table[first_y][first_x] = CapitalCell(first_x, first_y, Direction.BLUE, self)
+            self.table[second_y][second_x] = CapitalCell(second_x, second_y, Direction.ORANGE, self)
             return None
-        self.generate_capitals_coordinates(distance)
+        self.create_capitals(distance)
 
     def is_cell_can_be_captured(self, x: int, y: int):
         if self.table[y][x].direction == self.current_direction and type(self.table[y][x]) != EmptyCell:
@@ -233,46 +267,127 @@ class Board:
                         self.table[y][x] = cell.new_cell(x, y, self.current_direction, self)
                         self.change_current_direction()
 
-    def is_win(self):
-        is_blue_capital_alive = False
-        is_orange_capital_alive = False
+    def mouse_down_processing(self, mouse_position):
+        for cell in self.added:
+            x_range = range(cell.x, cell.x + self.cell_size)
+            y_range = range(cell.y, cell.y + self.cell_size)
+            if mouse_position[0] in x_range and mouse_position[1] in y_range:
+                cell.on_mouse_down(mouse_position)
         for row in self.table:
             for cell in row:
-                if type(cell) == CapitalCell:
-                    if cell.direction == Direction.BLUE:
-                        is_blue_capital_alive = True
-                    else:
-                        is_orange_capital_alive = True
-        if is_orange_capital_alive and is_blue_capital_alive:
+                if issubclass(type(cell), ClickableCell):
+                    x_range = range(cell.x, cell.x + self.cell_size)
+                    y_range = range(cell.y, cell.y + self.cell_size)
+                    if mouse_position[0] in x_range and mouse_position[1] in y_range:
+                        cell.on_mouse_down(mouse_position)
+
+    def mouse_up_processing(self, mouse_position):
+        for cell in self.added:
+            if cell.is_draggable:
+                self.add_new_cell(cell)
+            cell.on_mouse_up()
+        for row in self.table:
+            for cell in row:
+                if issubclass(type(cell), ClickableCell):
+                    x_range = range(cell.x, cell.x + self.cell_size)
+                    y_range = range(cell.y, cell.y + self.cell_size)
+                    if mouse_position[0] in x_range and mouse_position[1] in y_range:
+                        cell.on_mouse_up()
+
+    def on_mouse_motion(self, mouse_position):
+        for cell in self.added:
+            cell.on_drag(mouse_position)
+
+
+class DeadBoard(DefaultBoard):
+    def change_current_direction(self):
+        self.delete_cell()
+        self.current_direction = directions[(directions.index(self.current_direction) + 1) % 2]
+        self.added = [AddingCell(x, 6, self.current_direction, self)
+                      for x in range(self.side_size + 2, self.side_size + 5)]
+
+    def is_cell_can_be_captured(self, x: int, y: int):
+        if type(self.table[y][x]) == DeadCell:
+            return False
+        if self.table[y][x].direction == directions[(directions.index(self.current_direction) + 1) % 2]:
+            if issubclass(type(self.table[y][x]), ProtectedCell):
+                return False
+        neighbors = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+        for neighbor in neighbors:
+            neighbor_x, neighbor_y = neighbor
+            if neighbor_x in range(self.side_size) and neighbor_y in range(self.side_size):
+                neighbor_cell = self.table[neighbor_y][neighbor_x]
+                if neighbor_cell.direction == self.current_direction:
+                    return True
+        return False
+
+    def add_new_cell(self, cell: AddingCell):
+        for x in range(self.side_size):
+            for y in range(self.side_size):
+                table_cell = self.table[y][x]
+                if self.is_cell_can_be_captured(x, y):
+                    x_diff = abs(table_cell.x - cell.draw_x)
+                    y_diff = abs(table_cell.y - cell.draw_y)
+                    if x_diff < self.cell_size // 3 and y_diff < self.cell_size // 3:
+                        self.table[y][x] = cell.new_cell(x, y, self.current_direction, self)
+                        self.change_current_direction()
+
+    def delete_cell(self):
+        x, y = random.choices(range(self.side_size), k=2)
+        if self.table[y][x].direction == Direction.NOBODY:
+            self.table[y][x] = DeadCell(x, y, Direction.ORANGE, self)
             return None
-        else:
-            if is_orange_capital_alive:
-                self.winner = Direction.ORANGE
-            else:
-                self.winner = Direction.BLUE
+        empties = 0
+        for row in self.table:
+            for cell in row:
+                if cell.direction == Direction.NOBODY:
+                    empties += 1
+        if empties > 0:
+            self.delete_cell()
 
 
-class GameManager(Board):
-    def __init__(self, side_size: int):
+all_game_modes = [DefaultBoard, DeadBoard]
+start_sprites = pygame.sprite.Group()
+
+
+class GameManager:
+    def __init__(self):
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        super().__init__(side_size)
         self.is_game_process = False
-        self.clock = pygame.time.Clock()
-        self.fps = 60
-
-    def initialize(self):
-        self.generate_capitals_coordinates(distance=3)
-        self.create_capitals()
+        self.board = None
 
     def start(self):
-        self.is_game_process = True
-        self.initialize()
-        self.play()
+        game_mode_id = 0
+        self.start_init()
+        while not self.is_game_process:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    game_mode_id = (game_mode_id + 1) % len(all_game_modes)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.is_game_process = True
+                        self.board = all_game_modes[game_mode_id](8)
+                        self.play()
+                        return None
+            self.screen.fill(pygame.Color(27, 27, 27))
+            start_sprites.draw(self.screen)
+            pygame.display.flip()
+
+    def start_init(self):
+        a = self.screen.get_size()[0] // 40
+        b = self.screen.get_size()[1] // 20
+        c = ["leftButton.png", "rightButton.png"]
+        f = self.screen.get_size()[0] // 3
+        for i in range(2):
+            button = pygame.sprite.Sprite()
+            button.image = load_image(c[i], color_key=-1)
+            button.image = pygame.transform.scale(button.image, (a, b))
+            button.rect = (i * self.screen.get_size()[0] // 3 + f, self.screen.get_size()[1] // 2)
+            start_sprites.add(button)
 
     def play(self):
         while self.is_game_process:
-            self.is_win()
-            if not self.winner:
+            if not self.board.is_win():
                 self.manage_events()
                 self.render()
             else:
@@ -294,35 +409,13 @@ class GameManager(Board):
                     self.finish()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    for cell in self.added:
-                        x_range = range(cell.x, cell.x + self.cell_size)
-                        y_range = range(cell.y, cell.y + self.cell_size)
-                        if event.pos[0] in x_range and event.pos[1] in y_range:
-                            cell.on_mouse_down(event.pos)
-                    for row in self.table:
-                        for cell in row:
-                            if issubclass(type(cell), ClickableCell):
-                                x_range = range(cell.x, cell.x + self.cell_size)
-                                y_range = range(cell.y, cell.y + self.cell_size)
-                                if event.pos[0] in x_range and event.pos[1] in y_range:
-                                    cell.on_mouse_down(event.pos)
+                    self.board.mouse_down_processing(event.pos)
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
-                    for cell in self.added:
-                        if cell.is_draggable:
-                            self.add_new_cell(cell)
-                        cell.on_mouse_up()
-                    for row in self.table:
-                        for cell in row:
-                            if issubclass(type(cell), ClickableCell):
-                                x_range = range(cell.x, cell.x + self.cell_size)
-                                y_range = range(cell.y, cell.y + self.cell_size)
-                                if event.pos[0] in x_range and event.pos[1] in y_range:
-                                    cell.on_mouse_up()
+                    self.board.mouse_up_processing(event.pos)
             if event.type == pygame.MOUSEMOTION:
-                for cell in self.added:
-                    cell.on_drag(event.pos)
+                self.board.on_mouse_motion(event.pos)
 
 
-GAME = GameManager(side_size=8)
-GAME.start()
+game = GameManager()
+game.start()
